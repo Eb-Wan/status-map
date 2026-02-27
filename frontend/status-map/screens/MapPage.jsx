77ff231259796e4f957e230097eff2166cb6413d
CBalthazar <balthclement@gmail.com>
2026-02-27 11:03:28 +0100

    add location fetching

--- frontend/status-map/screens/MapPage.jsx
+++ frontend/status-map/screens/MapPage.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { StyleSheet, Text, View } from "react-native";
+
 const MapPage = () => {
-  return <div>MapPage</div>;
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync();
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = "waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else {
+    text = JSON.stringify(location);
+  }
+
+  return <View style={styles.container}></View>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+});
 
 export default MapPage;
